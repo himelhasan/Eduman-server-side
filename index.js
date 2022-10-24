938352ee32d6c27da1ca4aff5d57723fd5824c88
Himel Hasan <himelhasan497@gmail.com>
2022-10-25 00:18:26 +0600

    differnet level of api get method added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courseData = require("./data/data.json");
+
 app.get("/", (req, res) => {
   res.send("course Data Server is running");
 });
@@ -12,14 +13,45 @@ app.get("/", (req, res) => {
 app.get("/courses", (req, res) => {
   res.send(courseData);
 });
-app.get("*", (req, res) => {
-  res.send({ error: "please hit the right link" });
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const singleCourse = courseData.find((course) => course.id == id) || {};
+  res.send(singleCourse);
 });
 
-app.get("/courses/:id", (req, res) => {
-  const id = req.params.id;
-  const singleCourse = courseData.find((course) => course.id === id) || {};
-  res.send(singleCourse);
+app.get("/skillLevel/:skillLevel", (req, res) => {
+  const skillLevel = req.params.skillLevel;
+  const Level = courseData.filter(
+    (course) => course.skillLevel.toLowerCase() === skillLevel.toLowerCase()
+  );
+  res.send(Level);
+});
+
+app.get("/language/:language", (req, res) => {
+  const language = req.params.language;
+  const languageCourse = courseData.filter(
+    (course) => course.language.toLowerCase() === language.toLowerCase()
+  );
+  res.send(languageCourse);
+});
+
+app.get("/courses/:tags", (req, res) => {
+  const tags = req.params.tags;
+  const singleTags =
+    courseData.filter((course) => course.tags.toLowerCase() == tags.toLowerCase()) || {};
+  res.send(singleTags);
+});
+
+app.get("/:category", (req, res) => {
+  const category = req.params.category;
+  const singleCategory = courseData.filter((c) => c.category === category);
+  res.send(singleCategory);
+  console.log(category);
+});
+
+app.get("*", (req, res) => {
+  res.send({ error: "please hit the right link" });
 });
 
 app.listen(port, () => {
