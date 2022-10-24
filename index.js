5d743675dd4f433162dd85164c3b46f0991d69bb
Himel Hasan <himelhasan497@gmail.com>
2022-10-24 23:07:27 +0600

    server ready for action

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
 
+const courseData = require("./data/data.json");
 app.get("/", (req, res) => {
-  res.send("hey baby");
+  res.send("course Data Server is running");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courseData);
+});
+app.get("*", (req, res) => {
+  res.send({ error: "please hit the right link" });
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const singleCourse = courseData.find((course) => course.id === id) || {};
+  res.send(singleCourse);
 });
 
 app.listen(port, () => {
